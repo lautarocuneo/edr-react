76b0d048670251352e3fd854ee010385a24720e3
lautarocuneo <lautarocuneo@gmail.com>
2024-07-26 21:31:30 -0300

    actualizo errores

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -27,9 +27,9 @@ const NavBar = () => {
             <li className='p-4'>Contacto</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
-          {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>
-        <div className={!nav ? 'absolute left-0 top-20 w-[60%]  bg-[#202425] ease-in-out duration-700 z-0 ' : "absolute -translate-x-full ease-in-out duration-500 top-20 md:hidden"}>
+        <div className={nav ? 'absolute left-0 top-20 w-[60%]  bg-[#202425] ease-in-out duration-700 z-0 ' : "absolute -translate-x-full ease-in-out duration-500 top-20 md:hidden"}>
           <ul className='uppercase p-5 '>
             <li className='p-4 border-b border-[#2D3235]'>Home</li>
             <li className='p-4 border-b border-[#2D3235]'>Rodaje</li>
