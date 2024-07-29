8e0b1665d2287aff994e341c494735b1e22ffe15
lautarocuneo <lautarocuneo@gmail.com>
2024-07-29 14:02:14 -0300

    boludeces
    xdd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Equipos from "./components/Equipos";
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Hero/>
+      <Equipos/>
     </div>
   );
 }
--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ReactTyped } from "react-typed";
 import IconAnimation from './IconAnimation';
 
+import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 
 
 
@@ -14,7 +15,7 @@ const Hero = () => {
         <p className='text-[#2A86E2] fond-bold p-2 md:text-3xl'>ALQUILA TUS EQUIPOS</p>
         <h1 className='md:text-6xl sm:text-5xl text-3xl font-bold md:py-6 py-3 mx-3'>Rental de equipos de rodaje y utilería</h1>
         <div>
-          <p className='md:text-4xl sm:text-3xl text-xl font-medium md:pb-8'>Revisa nuestro catálogo y armá tu pedido</p>
+          <p className='md:text-4xl sm:text-3xl text-xl font-medium md:pb-8 text-[#b3b6b7]'>Revisa nuestro catálogo y armá tu pedido</p>
           <div className='flex flex-col justify-center items-center'>
           <ReactTyped
           className='md:text-5xl sm:text-4xl text-xl font-bold pb-3'
@@ -23,6 +24,12 @@ const Hero = () => {
             backSpeed = {140} loop/>
           <IconAnimation/>
           </div>
+        </div>
+        
+      </div>
+      <div className="">
+        <div className="absolute bottom-4 right-4">
+          <MdKeyboardDoubleArrowDown size={60} color={"#2A86E2"} />
         </div>
       </div>
     </div>
