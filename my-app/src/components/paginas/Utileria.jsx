fad4a3b5022eada75cef4d5716b47ba7e2680826
lautarocuneo <lautarocuneo@gmail.com>
2025-11-06 18:18:37 -0300

    asdsad
    asdsad

--- my-app/src/components/paginas/Utileria.jsx
+++ my-app/src/components/paginas/Utileria.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import SlideCarousel from "../ui/SlideCarousel";
 import NavBarUtileria from "../NavBarUtileria";
 import FooterUtileria from "../FooterUtileria";
